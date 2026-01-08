0f9c02c4c641a521a603cf7d20e658dfe9b7c40f
abuzar <sayyedabuzar021@gmail.com>
2026-01-08 23:35:27 +0530

    fixed base url

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://abuzarsayyed.vercel.app' // Replace with your actual domain if different
+    const baseUrl = 'https://abuzarsayyed // Replace with your actual domain if different
 
     return [
         {
