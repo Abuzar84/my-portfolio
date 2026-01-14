5c6527adffe2bde2ed01d5358e3b7c6bb89e0933
abuzar <sayyedabuzar021@gmail.com>
2026-01-14 21:11:30 +0530

    sitemap updated

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -14,13 +14,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
             url: `${baseUrl}/pdf-editor`,
             lastModified: new Date(),
             changeFrequency: 'weekly',
-            priority: 0.8,
+            priority: 0.9,
         },
         {
             url: `${baseUrl}/privacy-policy`,
             lastModified: new Date(),
             changeFrequency: 'yearly',
-            priority: 0.3,
+            priority: 0.8,
         },
         {
             url: `${baseUrl}/hoteltheme`,
