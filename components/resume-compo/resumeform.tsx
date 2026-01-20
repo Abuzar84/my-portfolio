9200d09f81f07542ee8e68c48b668993fc3ae112
abuzar <sayyedabuzar021@gmail.com>
2026-01-20 09:54:18 +0530

    form working is started

--- components/resume-compo/resumeform.tsx
+++ components/resume-compo/resumeform.tsx
@@ -1,14 +1,24 @@
 export default function ResumeForm() {
     return (
         <div className="flex justify-center items-center h-screen w-screen">
-            <form className="w-full">
-                <div className="flex">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" id="name" name="name" placeholder="Enter your full name" />
-                </div>
-                <label htmlFor="email">Email</label>
+            <form className="flex flex-wrap w-[50vw] h-[50vh]">
+                <label htmlFor="name">Name</label>&ensp;
+                <input type="text" id="name" name="name" placeholder="Enter your full name" />
+                <label htmlFor="email">Email</label>&ensp;
                 <input type="email" id="email" name="email" placeholder="Enter your email" />
-            </form >
+                <label htmlFor="phone">Phone</label>&ensp;
+                <input type="text" id="phone" name="phone" placeholder="Enter your phone number" />
+                <label htmlFor="address">Address</label>&ensp;
+                <input type="text" id="address" name="address" placeholder="Enter your address" />
+                <label htmlFor="summary">Summary</label>&ensp;
+                <input type="text" id="summary" name="summary" placeholder="Enter your summary" />
+                <label htmlFor="experience">Experience</label>&ensp;
+                <input type="text" id="experience" name="experience" placeholder="Enter your experience" />
+                <label htmlFor="education">Education</label>&ensp;
+                <input type="text" id="education" name="education" placeholder="Enter your education" />
+                <label htmlFor="skills">Skills</label>&ensp;
+                <input type="text" id="skills" name="skills" placeholder="Enter your skills" />
+            </form>
         </div >
     );
 }
